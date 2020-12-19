af61bb6f8fc29cdff15d9de67f22a9092f196894
Tanadon Sangkhatorn <tanadon.san@student.mahidol.edu>
2020-12-20 01:20:49 +0700

    Added /register, /check-in, /booking, /payment Page

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, This is the Main page!"
+
+@app.route("/register")
+def register():
+    return "Hello, This is the Register page!"
+
+@app.route("/booking")
+def booking():
+    return "Hello, This is the Booking page!"
+
+@app.route("/check-in")
+def checkin():
+    return "Hello, This is the Checking-in page!"
+
+@app.route("/payment")
+def payment():
+    return "Hello, This is the Payment page!"
